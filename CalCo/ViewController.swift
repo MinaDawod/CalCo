7a6632ba502952586e67fd0d9dd7b7ee89626964
MinaDawod <140282078+MinaDawod@users.noreply.github.com>
2024-05-25 10:28:02 +0300

    Initial Commit

---
+++ CalCo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalCo
+//
+//  Created by Mina Dawood on 25/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CalCoUITests/CalCoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CalCoUITestsLaunchTests.swift
+//  CalCoUITests
+//
+//  Created by Mina Dawood on 25/05/2024.
+//
+
+import XCTest
+
+final class CalCoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
